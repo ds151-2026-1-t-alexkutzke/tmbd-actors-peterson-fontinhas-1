b0663dcd6155ba0917aec6e31a59e23d0498609e
Peterson Fontinhas <petersonfontinhas@gmail.com>
2026-04-01 18:03:46 -0300

    Elenco adicionado. Informações do ator sendo implementadas

--- app/actor/[id].tsx
+++ app/actor/[id].tsx
@@ -2,7 +2,6 @@ import { Link, useLocalSearchParams } from 'expo-router';
 import { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, ActivityIndicator, ScrollView, FlatList, Pressable } from 'react-native';
 import { api } from '../../src/api/tmdb';
-import CastCard from './cast_card';
 
 interface MovieDetails {
   title: string;
@@ -23,20 +22,22 @@ export interface CastDetails{
 export default function MovieDetailsScreen() {
   // Captura o parâmetro '[id]' do nome do arquivo
   const { id } = useLocalSearchParams();
-  const [movie, setMovie] = useState<MovieDetails | null>(null);
-  const [cast, setCast] = useState<CastDetails[] | null>([]);
+  const [actor, setActor] = useState<MovieDetails | null>(null);
+  const [movies, setMovies] = useState<CastDetails[] | null>([]);
   const [isLoading, setIsLoading] = useState(true);
   
-  console.log("TAMANHO TOTAL DO ELENCO: ", cast!.length);
+  console.log("TAMANHO TOTAL DO ELENCO: ", movies!.length);
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
-        const movieDetails = await api.get(`/movie/${id}`);
-        setMovie(movieDetails.data);
+        const actorDetails = await api.get(`/person/${id}`);
+        setActor(actorDetails.data);
 
-        const castDetails = await api.get(`/movie/${id}/credits`);
+        const actorMovieDetails = await api.get(`/person/${id}/movi`);
 
-        setCast(castDetails.data['cast']);
+        console.log("Filmes do ator", actorMovieDetails.data)
+
+        setMovies(actorMovieDetails.data['cast']);
 
       } catch (error) {
         console.error('Erro ao buscar detalhes:', error);
@@ -56,7 +57,7 @@ export default function MovieDetailsScreen() {
     );
   }
 
-  if (!movie) {
+  if (!actor) {
     return (
       <View style={styles.center}>
         <Text style={styles.errorText}>Filme não encontrado.</Text>
@@ -66,44 +67,44 @@ export default function MovieDetailsScreen() {
 
   return (
     <ScrollView style={styles.container}>
-      {movie.poster_path && (
+      {actor.poster_path && (
         <Image
           source={{
-            uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            uri: `https://image.tmdb.org/t/p/w500${actor.poster_path}`,
           }}
           style={styles.poster}
           resizeMode="cover"
         />
       )}
       <View style={styles.content}>
-        <Text style={styles.title}>{movie.title}</Text>
+        <Text style={styles.title}>{actor.title}</Text>
 
         <View style={styles.statsContainer}>
           <Text style={styles.statText}>
-            ⭐ {movie.vote_average.toFixed(1)}/10
+            ⭐ {actor.vote_average.toFixed(1)}/10
           </Text>
-          <Text style={styles.statText}>⏱️ {movie.runtime} min</Text>
+          <Text style={styles.statText}>⏱️ {actor.runtime} min</Text>
         </View>
 
         <Text style={styles.sectionTitle}>Sinopse</Text>
         <Text style={styles.overview}>
-          {movie.overview || "Sinopse não disponível para este filme."}
+          {actor.overview || "Sinopse não disponível para este filme."}
         </Text>
 
         <Text style={styles.sectionTitle}>Elenco</Text>
-        <FlatList
+        {/* <FlatList
           data={cast}
           horizontal={true}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
 
-            <Link href={`/actor/${item.id}`} asChild>            
+            <Link href={'/'} asChild>            
               <Pressable>
                 <CastCard {...item}></CastCard>
               </Pressable>
             </Link>
           )}
-        />
+        /> */}
       </View>
     </ScrollView>
   );
